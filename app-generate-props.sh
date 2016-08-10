bb3124a9edf0f51ea41da57eea35214afc03f1be
kiwi <jcwen@thoughtworks.com>
2016-08-10 22:11:37 +0800

    overwrite properties if property defined in existing property file

--- app-generate-props.sh
+++ app-generate-props.sh
@@ -7,6 +7,8 @@ do
 		propKey=`echo "$VAR" | sed 's/APP_.*_PROPS_\(.*\)=.*/\1/' | tr '[:upper:]' '[:lower:]' | tr _ .`
 		propValue=`echo "$VAR" | sed 's/APP_.*_PROPS_.*=\(.*\)/\1/'`
 		
+		#remove existing key,value pair
+		sed "/^${propKey}=/"d $APP_HOME/config/$propFile.properties >> $APP_HOME/config/$propFile.properties
 		echo "$propKey=$propValue" >> $APP_HOME/config/$propFile.properties
 	fi
 done
